daa3ba4499d1098cd88eb67f3d9799cb30a7132b
Animesh <animeshsingh16121@gmail.com>
2026-03-12 12:01:46 +0900

    Update conftest for teh env variables

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
 import os
 
+# Provide dummy values so Settings() can be instantiated during unit tests.
+# All external API calls are mocked in individual tests; these values never reach real services.
+os.environ.setdefault("GOOGLE_API_KEY", "dummy-for-tests")
+os.environ.setdefault("QDRANT_URL", "http://localhost:6333")
+os.environ.setdefault("QDRANT_API_KEY", "dummy-for-tests")
+os.environ.setdefault("COHERE_API_KEY", "dummy-for-tests")
+os.environ.setdefault("LANGSMITH_API_KEY", "dummy-for-tests")
+os.environ.setdefault("LANGSMITH_PROJECT", "test-project")
+
 # Disable LangSmith tracing during tests to avoid polluting production traces
 os.environ["LANGSMITH_TRACING"] = "false"
